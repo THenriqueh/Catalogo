d300f93f4f2927e3bc5bdb8ef2bce5cec219420e
Thallis Ferreira <thallis.ferreira2019@gmail.com>
2023-06-05 15:36:24 -0300

    database

--- src/main/java/com/freemarcket/catalogo/services/CategoryService.java
+++ src/main/java/com/freemarcket/catalogo/services/CategoryService.java
@@ -5,6 +5,7 @@ import com.freemarcket.catalogo.entities.Category;
 import com.freemarcket.catalogo.repositories.CategoryRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ public class CategoryService {
     @Autowired
     private CategoryRespository respository;
 
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return respository.findAll();
     }
